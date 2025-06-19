e502b007c3418f9c2d39df0cbc0ccd534253c2ae
Naveen Kumar Sanmugasundaram <naveenkumar.s@htcinc.com>
2025-06-19 16:35:17 +0530

    works

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-sudo cp /home/ec2-user/demo-app/index.html /var/www/html/index.html
+#set -e
+
+#echo "==> Starting ApplicationStart hook" | sudo tee -a /tmp/deploy.log
+
+# Stop Apache if running
+sudo systemctl stop httpd || echo "httpd not running"
+
+# Clear old files (optional)
+sudo rm -rf /var/www/html/demo-app1/*
+
+# Copy updated files
+sudo cp -R /home/ec2-user/demo-app/* /var/www/html/demo-app1/
+
+# Start Apache
 sudo systemctl start httpd
 sudo systemctl enable httpd
