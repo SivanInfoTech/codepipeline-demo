31812bfd7b271ba81123dfc0455ecb9a058341f8
SivanInfoTech <132988914+SivanInfoTech@users.noreply.github.com>
2025-06-19 16:10:06 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-sudo cp /home/ec2-user/demo-app/index.html /var/www/html/index.html
+
+# Stop Apache before making changes
+sudo systemctl stop httpd
+
+# Clean old files (optional but recommended for static sites)
+sudo rm -rf /var/www/html/*
+
+# Copy all files from deployed folder
+sudo cp -r /home/ec2-user/demo-app/* /var/www/html/
+
+# Start Apache
 sudo systemctl start httpd
 sudo systemctl enable httpd
