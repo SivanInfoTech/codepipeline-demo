d759c66abeeb339aca31faebecdd4c97589c7aee
SivanInfoTech <132988914+SivanInfoTech@users.noreply.github.com>
2025-06-19 17:14:12 +0530

    Update post.sh

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -e
 
 echo "PostScript ran at $(date)" > /tmp/postscript.log
+
+# Copy updated HTML file
+sudo cp /home/ec2-user/demo-app/index.html /var/www/html/index.html
+
+# Restart Apache to reflect changes
+sudo systemctl restart httpd
